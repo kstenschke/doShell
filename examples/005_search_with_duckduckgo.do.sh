88ce4a1711e983bb18f557af19d3b21deb50ad43
kay.stenschke <stenschke@gyselroth.com>
2020-07-29 17:35:04 +0200

    correct example syntax

--- examples/005_search_with_duckduckgo.do.sh
+++ examples/005_search_with_duckduckgo.do.sh
@@ -4,14 +4,5 @@
 sleep 1
 
 #open url in new browsertab: "https://duckduckgo.com/"
-
-
-
-#open new browserTab
-sleep 0.3
-#focus browserURL
-sleep 0.3
-#type "https://www.github.com/kstenschke"
-sleep 0.5
+#type "what is new?"
 #hit enter
-
