2674791a55259874f797f277282ed1efcffb072f
Kay Stenschke <info@stenschke.com>
2020-07-29 22:21:55 +0200

    Update 004_load_url_in_new_browser_tab.do.sh

--- examples/004_load_url_in_new_browser_tab.do.sh
+++ examples/004_load_url_in_new_browser_tab.do.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#activate browser; wait 1
-#open new browserTab; wait .5
+#activate browser #wait 1
+#open new browserTab #wait .5
 #focus browserURL
-#type "https://www.github.com"; wait .5
+#type "https://www.github.com" #wait .5
 #hit enter
