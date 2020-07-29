d7300eda99e44fe37bfbd02bfe23d58bf7924825
Kay Stenschke <info@stenschke.com>
2020-07-29 20:21:11 +0200

    Update 004_load_url_in_new_browser_tab.do.sh

--- examples/004_load_url_in_new_browser_tab.do.sh
+++ examples/004_load_url_in_new_browser_tab.do.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-#activate browser
-sleep 1
-#open new browserTab
-sleep 0.3
+#activate browser; wait 1
+#open new browserTab; wait .5
 #focus browserURL
-sleep 0.3
-#type "https://www.github.com/kstenschke"
-sleep 0.5
+#type "https://www.github.com"; wait .5
 #hit enter
