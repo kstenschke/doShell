5873fdce3a2d120ba93c2870429d77657d7d51bb
kay.stenschke <stenschke@gyselroth.com>
2020-08-31 23:21:26 +0200

    added runtime condition and -flag to example

--- examples/004_search_with_duckduckgo.do.sh
+++ examples/004_search_with_duckduckgo.do.sh
@@ -3,6 +3,12 @@
 # Load duckduckgo in new browser tab,
 # enter a search term and invoke the search
 
+#IF(LINUX)
+  #BROWSER=chromium
+#ELSEIF(MAC)
+  #BROWSER=firefox
+#ENDIF
+
 #activateBrowser
 #openUrlInNewBrowserTab "https://duckduckgo.com/"
 
