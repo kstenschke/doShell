7dd626a7bc1c99b8bc8f3c81d22393f2c346ae48
Kay Stenschke <info@stenschke.com>
2020-10-05 13:38:03 +0200

    Create 004_random.sh

---
+++ test/functional/004_random.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bats
+
+load _helper
+
+# TODO assert output of random number is numeric and within given range
+
+# TODO assert repeated output of random number varies
+
+# TODO assert random number output from within platform condition
+
+# TODO assert random number from imported script
