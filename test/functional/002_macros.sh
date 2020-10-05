fee325825692c1e94ced5c325a4c34bac1d16c27
Kay Stenschke <info@stenschke.com>
2020-10-05 03:01:17 +0200

    Create 002_macros.sh

---
+++ test/functional/002_macros.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bats
+
+load _helper
+
+# TODO assert output contains parsed ::DIR::
+
+# TODO assert output contains parsed ::DIR_DOSH::
+
+# TODO assert output contains parsed ::DIR_EXEC::
+
+# TODO assert output contains parsed ::FILE::
+
+# TODO assert output contains parsed ::LINE::
+
+# TODO assert output contains parsed ::TIMESTAMP::
+
+# TODO assert output of imported sub-script contains parsed macros
