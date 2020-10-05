15b43a6c11b9308ea246565cc558962ed9d103ce
Kay Stenschke <info@stenschke.com>
2020-10-05 03:11:38 +0200

    Create 003_platform_conditions.sh

---
+++ test/functional/003_platform_conditions.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bats
+
+load _helper
+
+# TODO assert output of plaform condition is of current and not other platform
+
+# TODO assert macros within platform condition
+
+# TODO assert import within platform condition
+
+# TODO assert platform condition within import
