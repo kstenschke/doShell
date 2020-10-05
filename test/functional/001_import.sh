6ad26536314ba64a6b1bdf7181d4776c8c6ec0c4
Kay Stenschke <info@stenschke.com>
2020-10-05 02:57:31 +0200

    Update 001_import.sh

--- test/functional/001_import.sh
+++ test/functional/001_import.sh
@@ -2,8 +2,18 @@
 
 load _helper
 
-@test "$BATS_TEST_NUMBER: \"#import - imports given file" {
-  run "${DOSH_BINARY}" -rc "examples/001_import.do.sh"
-  [ "$status" -ne 0 ]
-# TODO assert import worked
+# TODO assert output from imported sub-script shows up in execution output
+test "$BATS_TEST_NUMBER: \"#import - imports given file" {
+#  run "${DOSH_BINARY}" -rc "examples/001_import.do.sh"
+#  [ "$status" -ne 0 ]
 }
+
+# TODO assert output of imported sub-script contains filename
+
+# TODO assert output of imported sub-script contains line number
+
+# TODO assert output of imported sub-script contains parsed macros
+
+# TODO assert output of imported sub-script contains parsed condition
+
+# TODO assert output of imported sub-script contains parsed runtime parameters
