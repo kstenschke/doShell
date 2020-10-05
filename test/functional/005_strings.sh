d892186cad43b4b273ef84569d4afd189e7cc298
Kay Stenschke <info@stenschke.com>
2020-10-05 13:42:24 +0200

    Create 005_strings.sh

---
+++ test/functional/005_strings.sh
@@ -0,0 +1,48 @@
+
+#!/usr/bin/env bats
+
+# todo test extractBetween
+
+# todo test replaceAfter
+
+# todo test replaceAll
+
+# todo test replaceBefore
+
+# todo replaceBetween
+
+# todo test replaceFirst
+
+# todo test replaceLast 
+
+# --- test string commands from imported script:
+
+# todo test extractBetween from imprted script
+
+# todo test replaceAfter from imprted script
+
+# todo test replaceAll from imprted script
+
+# todo test replaceBefore from imprted script
+
+# todo replaceBetween from imprted script
+
+# todo test replaceFirst from imprted script
+
+# todo test replaceLast  from imprted script
+
+# --- test string commands from within platform condition:
+
+# todo test extractBetween from within platform condition
+
+# todo test replaceAfter from within platform condition
+
+# todo test replaceAll from within platform condition
+
+# todo test replaceBefore from within platform condition
+
+# todo replaceBetween from within platform condition
+
+# todo test replaceFirst from within platform condition
+
+# todo test replaceLast from within platform condition
