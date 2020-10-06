dcc444261e4abfc29197e97c63645a27fc8c4414
kstenschke <info@stenschke.com>
2020-10-06 08:39:28 +0200

    added (failing) string manipulation examples

--- examples/005_strings.do.sh
+++ examples/005_strings.do.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # All string helper command examples output: foo bar baz qux
 
+echo
+echo "1. Manipulate hard-coded strings"
+echo "--------------------------------"
+
 printf "extractBetween: "
 echo #extractBetween "start foo bar baz qux end" start end
 
@@ -21,3 +25,38 @@ echo #replaceFirst "bar bar baz qux" bar foo
 
 printf "\nreplaceLast: "
 echo #replaceLast "foo bar baz bar" bar qux
+
+echo
+echo
+echo "2. Manipulate strings from variables"
+echo "------------------------------------"
+
+printf "extractBetween: "
+STR="start foo bar baz qux end"
+echo #extractBetween $STR start end
+
+printf "\nreplaceAfter: "
+STR="foo bar xxx xxx"
+echo #replaceAfter $STR bar "bar baz qux"
+
+printf "\nreplaceAll: "
+STR="foo ber bez qux"
+echo #replaceAll $STR e a
+
+printf "\nreplaceBefore: "
+STR="thingummibob bar baz qux"
+echo #replaceBefore $STR bar "foo bar"
+
+printf "\nreplaceBetween: "
+STR="foo bar thingummibob qux"
+echo #replaceBetween $STR bar qux " bar baz "
+
+printf "\nreplaceFirst: "
+STR="bar bar baz qux"
+echo #replaceFirst $STR bar foo
+
+printf "\nreplaceLast: "
+STR="foo bar baz bar"
+echo #replaceLast $STR bar qux
+
+echo
